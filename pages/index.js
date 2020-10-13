c4db26ab97605cbfa7be9d03adfbed9a043c7160
jose-donato <zmcdonato@gmail.com>
2020-10-13 20:25:03 +0100

    reverted

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <img
-        src="https://i.pinimg.com/originals/a1/43/7b/a1437b982507ae0359f72fcb22edce6f.jpg"
-        width={400}
-        height={250}
-      />
-      <img
-        src="https://cdn.e-konomista.pt/uploads/2019/07/113488-berner-sennenhund-dog_1548030555-425x318.jpg"
-        width={400}
-        height={250}
-      />
-      <img
-        src="https://i.pinimg.com/originals/a8/13/32/a813322e172ffba5abbd78928a29d390.jpg"
-        width={400}
-        height={250}
-      />
-    </div>
-  );
+  return <h1>with-service-worker</h1>;
 }
